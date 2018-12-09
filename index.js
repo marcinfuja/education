c3282043cd22b3bf50fea0f0318de6f35042acd4
Marcin Fuja <mfuja@pgs-soft.com>
2018-12-09 20:57:45 +0100

    promise all, race

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
 (function() {
     'use strict';
 
-    // #region Setup
     const API_URL = "https://starwars.egghead.training/";
-    const output = document.getElementById('root')
-    const spinner = document.getElementById('spinner')
 
-    function getFilmTitles(films) {
-      return films
-        .sort((a,b) => a.episode_id - b.episode_id)
-        .map(film => `${film.episode_id}. ${film.title}`)
-        .join("\n");
-    }
-    // endregion
+    const output = document.getElementById('output');
+    const spinner = document.getElementById('spinner');
 
-    fetch(API_URL + "movies")
-      .then(response => {
-        if (!response.ok) {
-          return Promise.reject(
-            new Error("unsuccessful response")
-          )
-        }
-        return response.json().then(films => {
-          output.innerText = getFilmTitles(films);
-        })
-      })
-      .catch(error => {
-        console.warn(error);
-        output.innerText = ":(";
-      })
-      .finally(() => {
-        spinner.remove();
+    function queryAPI(endpoint) {
+      return fetch(API_URL + endpoint).then(response => {
+        return response.ok
+          ? response.json()
+          : Promise.reject(Error("Unsuccessful response"));
       });
+    }
+
+    const promise = Promise.all([
+      queryAPI('films'),
+      queryAPI('planets')
+    ])
+
+    promise.then(([films, planets]) => {
+      output.innerText =
+        `${films.length} films, ` +
+        `${planets.length} planets`
+    })
+    .finally(() => {
+      spinner.remove();
+    });
+
+
 
 })();
