0096454173b29ec1cfbf78cb7f9011c9d72f2d6e
Marcin Fuja <mfuja@pgs-soft.com>
2018-12-09 21:09:13 +0100

    clear index.js

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
 (function() {
     'use strict';
 
-    const API_URL = "https://starwars.egghead.training/";
-    const output = document.getElementById('root')
-    output.innerText = "Loading..."
-
-    fetch(API_URL + "films")
-      .then(response => {
-        if (!response.ok) {
-          throw Error('Unssucessful response');
-        }
-        return response.json().then(films => {
-          output.innerText = getFilmTitles(films);
-        })
-      })
-      .catch(error => {
-        console.warn(error);
-        output.innerText = ":(";
-      });
-
-    function getFilmTitles(films) {
-      return films
-        .sort((a,b) => a.episode_id - b.episode_id)
-        .map(film => `${film.episode_id}. ${film.title}`)
-        .join("\n");
-    }
 })();
