da8b94e5dd975798c466e44f6965af60ac6d133e
Marcin Fuja <mfuja@pgs-soft.com>
2018-12-10 19:21:51 +0100

    login route with authenticaton; install jwt and bodyparser

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const PORT = process.env.PORT || 8888;
+const users = [
+  {
+    id: 1,
+    username: "admin",
+    password: "admin"
+  },
+  {
+    id: 2,
+    username: "guest",
+    password: "guest"
+  }
+]
 
-app.get('/status', (req, res) => {
-  const localTime = (new Date()).toLocaleTimeString();
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    res
+    .status(400)
+    .send("You need a username and password");
+    return;
+  }
+
+  const user = users.find((user) => {
+    return user.username === req.body.username && user.password === req.body.password;
+  });
+
+  if (!user) {
+    res
+    .status(401)
+    .send("User not found")
+    return;
+  }
+
+  const token = jwt.sign({
+    sub: user.id,
+    username: user.username
+  }, "mysupersecretkey", {expiresIn: "3 hours"});
 
   res
-    .status(200)
-    .send(`Server time is ${localTime}`)
+  .status(200)
+  .send({access_token: token});
 })
 
 app.get('*', (req, res) => {
