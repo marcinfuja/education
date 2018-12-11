26f4d33c357d9a70d30437fa84ff34e0ce227ea2
Marcin Fuja <mfuja@pgs-soft.com>
2018-12-11 20:31:09 +0100

    update packages; send queries to server; add parameters to query and strong type them

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
 import React, { Component } from "react";
-import ApolloClient from "apollo-boost";
-import gql from "graphql-tag";
-import { ApolloProvider, ApolloConsumer } from "react-apollo";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import Recipes from './RecipesComponent';
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/"
 });
 
-// client
-//   .query({
-//     query: gql`
-//       {
-//         recipes {
-//           id
-//           title
-//         }
-//       }
-//     `
-//   })
-//   .then(result => console.log(result));
+
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
         <div>Hello World!</div>
-        <ApolloConsumer>
-          {client => {
-            client
-              .query({
-                query: gql`
-                  {
-                    recipes {
-                      id
-                      title
-                    }
-                  }
-                `
-              })
-              .then(result => console.log(result));
-            return null;
-          }}
-        </ApolloConsumer>
+        <Recipes />
       </ApolloProvider>
     );
   }
---
+++ src/RecipesComponent.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const recipesQuery = gql`
+    query recipes($vegetarian: Boolean!) {
+        recipes(vegetarian: $vegetarian) {
+            id
+            title
+        }
+    }
+`;
+
+export default class Recipes extends Component {
+
+    state = {
+        vegetarian: false
+    }
+
+    updateVegetarian = ({ target: {checked}}) => {
+        this.setState({vegetarian: checked})
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <label>
+                    <input type="checkbox" checked={this.state.vegetarian}
+                        onChange={this.updateVegetarian} />
+                    <span>vegetarian</span>
+                </label>
+                <Query 
+                    query={recipesQuery} 
+                    variables={{vegetarian: this.state.vegetarian}}
+                >
+                    {({data, loading, error}) => {
+                        if (loading) return <p>Loading...</p>;
+                        if (error) return <p>Error</p>;
+                        if (data.recipes === undefined) return null;
+                    
+                        return (
+                        <ul>
+                            {data.recipes.map(({id,title}) => 
+                            <li key={id}>{title}</li>
+                            )}
+                        </ul>
+                        )
+                    }}
+                </Query>
+            </React.Fragment>
+        )
+    }
+
+}
